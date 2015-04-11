a9e9e51aa71dfa604cc43454719502cc32f791df
Blake Brown <blakebrown129@gmail.com>
2015-04-11 17:14:02 -0400

    Get node to render index.html, rearrange folder structure

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
-var indico = require('indico.io');
+var express = require('express'),
+	path	= require("path"),
+	app 	= express(),
+	http 	= require('http').Server(app),
+	bodyParser = require('body-parser'),
+	indico = require('indico.io');
+
 var settings = {
   "api_key": "04ad709a428e213f86e226d9610b2e86"
 };
 
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+http.listen(process.env.PORT || 3000, function() {
+	console.log('listening on port 3000');
+});
+
 var single = "Blog posts about Android tech make better journalism than most news outlets.";
 indico.textTags(single, settings)
   .then(function(res) {
-    console.log(res);
+    //console.log(res);
   }).catch(function(err) {
     console.warn(err);
   });
@@ -18,7 +35,7 @@ var batch = [
 
 indico.batchTextTags(batch, settings)
   .then(function(res) {
-    console.log(res);
+    //console.log(res);
   }).catch(function(err) {
     console.warn(err);
   });
