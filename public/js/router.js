549ca64cf14bd134aa100ecdf6529030b840f0ff
kaustavha <hi@kaustav.me>
2015-04-11 22:18:32 -0400

    moved router outside of script.js

---
+++ public/js/router.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  // ROUTES
+  var index = function() {
+    console.log('welcome!');
+  };
+  var waitingRoom = function() {
+    console.log('Welcome to the waiting room');
+  };
+  var routes = {
+    '/home': index,
+    '/waitingRoom': waitingRoom
+  };
+
+  var allroutes = function() {
+    var route = window.location.hash.slice(2);
+    var sections = $('section');
+    var section;
+
+    section = sections.filter('[data-route=' + route + ']');
+
+    if (section.length) {
+      sections.hide(250);
+      section.show(250);
+    }
+  };
+  var router = Router(routes);
+  router.configure({
+    on: allroutes
+  });
+  router.init();
+  router.setRoute('/home');
+});
