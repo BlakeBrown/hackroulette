3b457004b09b4ff188ff70b6efeac26e495ae4c8
Blake Brown <blakebrown129@gmail.com>
2015-04-16 23:13:51 -0400

    Improve fading text on waiting room page

--- public/js/waiting-room/waiting-room.js
+++ public/js/waiting-room/waiting-room.js
@@ -33,30 +33,25 @@ $(document).ready(function() {
                 });
         });
 
-	var num = 0, style = document.getElementById('loadingText').style;
+    $(".start_button_solid").on("mouseenter", function() {
+        // Set the button to it's current opacity
+        var current_opacity = $(".start_button_solid").css("opacity");
+        $(".start_button_solid").css("opacity", current_opacity);
+        // Remove the animation
+        $(".start_button_solid").removeClass("animate-flicker");
+        // Fade the button to an opacity of 1
+        $(".start_button_solid").fadeTo("slow", 1);
+    });
 
-	window.setInterval(function () {
-
-		setTimeout(function() {
-		    $('.crystal').removeClass('fadeOut');
-		    $('.crystal').addClass('animated fadeIn').css('visibility', 'visible');
-		    $('.crystal').animate({transform: "translateY(-10%)"});
-		}, num);
-
-		num = num+800
-
-		setTimeout(function() {
-		    $('.crystal').removeClass('fadeIn');
-		    $('.crystal').addClass('fadeOut');
-		}, num);
-
-	}, 800);
+    $(".start_button_solid").on("mouseleave", function() {
+        $(".start_button_solid").addClass("animate-flicker");
+    });
 
 	setTimeout(function() {
 	    $('.loadingLine').addClass('animated pulse').css('visibility','visible');
 	},1000);
 
-	var items = Array("Is it ethical to get an abortion?", "Should be homeless be compensated?", "Does the government have a role in providing healthcare?", "Given our obsession with political correctness, does freedom of speech exist?", "How would you reform America's immigration policy?", "Should firearms be banned for public purchase in America?", "Should prisons be privately owned?", "Is America obligated to take a lead in solving global warming?", "Do you approve of Obama's current foreign policy?", "Should marijuana be legalized in the States?", "Does Chinese investment in America pose security threats?", "Does democracy stifle progress?");
-	topic = items[Math.floor(Math.random()*items.length)]
-	output = document.getElementById("output").innerHTML=topic;
+	// var items = Array("Is it ethical to get an abortion?", "Should be homeless be compensated?", "Does the government have a role in providing healthcare?", "Given our obsession with political correctness, does freedom of speech exist?", "How would you reform America's immigration policy?", "Should firearms be banned for public purchase in America?", "Should prisons be privately owned?", "Is America obligated to take a lead in solving global warming?", "Do you approve of Obama's current foreign policy?", "Should marijuana be legalized in the States?", "Does Chinese investment in America pose security threats?", "Does democracy stifle progress?");
+	// topic = items[Math.floor(Math.random()*items.length)]
+	// output = document.getElementById("output").innerHTML=topic;
 });
